f76906ce81b497500726e7ef8010c13db0e98a1a
Brent Vatne <brentvatne@gmail.com>
2018-06-06 18:01:15 -0700

    Run prettier with default config

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class ReadMore extends React.Component {
   state = {
     measured: false,
     shouldShowReadMore: false,
-    showAllText: false,
-  }
+    showAllText: false
+  };
 
   async componentDidMount() {
-    this._isMounted = true
+    this._isMounted = true;
     await nextFrameAsync();
 
-    if(!this._isMounted) {
-      return
+    if (!this._isMounted) {
+      return;
     }
 
     // Get the height of the text with no restriction on number of lines
     const fullHeight = await measureHeightAsync(this._text);
-    this.setState({measured: true});
+    this.setState({ measured: true });
     await nextFrameAsync();
 
-    if(!this._isMounted) {
-      return
+    if (!this._isMounted) {
+      return;
     }
 
     // Get the height of the text now that number of lines has been set
     const limitedHeight = await measureHeightAsync(this._text);
 
     if (fullHeight > limitedHeight) {
-      this.setState({shouldShowReadMore: true}, () => {
+      this.setState({ shouldShowReadMore: true }, () => {
         this.props.onReady && this.props.onReady();
       });
     } else {
@@ -42,24 +38,22 @@ export default class ReadMore extends React.Component {
   }
 
   componentWillUnmount() {
-    this._isMounted = false
+    this._isMounted = false;
   }
 
   render() {
-    let {
-      measured,
-      showAllText,
-    } = this.state;
+    let { measured, showAllText } = this.state;
 
-    let {
-      numberOfLines,
-    } = this.props;
+    let { numberOfLines } = this.props;
 
     return (
       <View>
         <Text
           numberOfLines={measured && !showAllText ? numberOfLines : 0}
-          ref={text => { this._text = text; }}>
+          ref={text => {
+            this._text = text;
+          }}
+        >
           {this.props.children}
         </Text>
 
@@ -69,18 +63,15 @@ export default class ReadMore extends React.Component {
   }
 
   _handlePressReadMore = () => {
-    this.setState({showAllText: true});
-  }
+    this.setState({ showAllText: true });
+  };
 
   _handlePressReadLess = () => {
-    this.setState({showAllText: false});
-  }
+    this.setState({ showAllText: false });
+  };
 
   _maybeRenderReadMore() {
-    let {
-      shouldShowReadMore,
-      showAllText,
-    } = this.state;
+    let { shouldShowReadMore, showAllText } = this.state;
 
     if (shouldShowReadMore && !showAllText) {
       if (this.props.renderTruncatedFooter) {
@@ -91,7 +82,7 @@ export default class ReadMore extends React.Component {
         <Text style={styles.button} onPress={this._handlePressReadMore}>
           Read more
         </Text>
-      )
+      );
     } else if (shouldShowReadMore && showAllText) {
       if (this.props.renderRevealedFooter) {
         return this.props.renderRevealedFooter(this._handlePressReadLess);
@@ -120,7 +111,7 @@ function nextFrameAsync() {
 
 const styles = StyleSheet.create({
   button: {
-    color: '#888',
-    marginTop: 5,
-  },
+    color: "#888",
+    marginTop: 5
+  }
 });
